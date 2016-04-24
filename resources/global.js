40f9381f8bdb03453a2405db141214fbe5376ec7
Joshua Diaddigo <joshuad95@hotmail.com>
2016-04-24 18:49:13 -0400

    commenting

--- resources/global.js
+++ resources/global.js
@@ -1,3 +1,13 @@
+// calling this function creates an alert window
+//
+// message: main content of alert
+// title: header of aler
+// button: text for alert button
+// cancel: boolean to show or hide cancel button
+// button_callback: function to run when button is pressed
+// cancel_callback: funcion to run when cancel is pressed
+// cancel_button_text: alternative text for cancel button
+
 function alert(message, title, button, cancel, button_callback, cancel_callback, cancel_button_text) {
     message = (message == null) ? "" : message;
     title = (title == null) ? "" : title;
@@ -30,6 +40,8 @@ function alert(message, title, button, cancel, button_callback, cancel_callback,
     select("id", "content").add_class("blurred");
 }
 
+
+// this function closes the current alert window. Should usually be called at the end of a button callback in alert()
 function close_alert() {
     select("id", "alert_container").add_class("transparent");
     setTimeout(function() {
@@ -39,6 +51,10 @@ function close_alert() {
     select("id", "content").remove_class("blurred");
 }
 
+// selects a dom element and adds some functionality to the returned result
+// such as add_class and remove_class
+// method can either be "id" or "class"
+// selecting a class will return an array of DOM_objects while an ID will return a single object
 function select(method, selector) {
     if (method == "id") {
         var js_object = document.getElementById(selector);
@@ -57,6 +73,7 @@ function select(method, selector) {
     }
 }
 
+// wrapper for a dom object to abstract out common functionality
 function DOM_Object(js_object) {
     this.js_object = js_object;
     if (this.className != undefined) {
@@ -65,6 +82,7 @@ function DOM_Object(js_object) {
         this.classes = [];
     }
 
+    //add a class to a dom object
     this.add_class = function (class_name) {
         if (this.classes.indexOf(class_name) == -1) {
             this.classes.push(class_name);
@@ -72,6 +90,7 @@ function DOM_Object(js_object) {
         this.js_object.className = this.classes.join(" ");
     };
 
+    // remove a class from a selected dom object
     this.remove_class = function(class_name) {
         if (this.classes.indexOf(class_name) != -1) {
             this.classes.splice(this.classes.indexOf(class_name), 1);
@@ -81,6 +100,11 @@ function DOM_Object(js_object) {
     return this;
 }
 
+// sends an htto get request
+// url: endpoint of request
+// data: a dictionary of values to send as the get vars
+// json_parse: boolean value for whether the result should be parsed as json
+// callback: function to call with the response as a var
 function get(url, data, json_parse, callback) {
     if (json_parse == undefined) json_parse = true;
     if (callback == undefined) callback = function(result) {};
@@ -112,6 +136,11 @@ function get(url, data, json_parse, callback) {
     request.send();
 }
 
+// sends an htto post request
+// url: endpoint of request
+// data: a dictionary of values to send as the post vars
+// json_parse: boolean value for whether the result should be parsed as json
+// callback: function to call with the response as a var
 function post(url, data, json_parse, callback) {
     if (json_parse == undefined) json_parse = true;
     if (callback == undefined) callback = function(result) {};
